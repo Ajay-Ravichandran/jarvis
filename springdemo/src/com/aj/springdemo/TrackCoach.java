d8359d45012e04235835fa5f0feba615853ca05b
Ajay-Ravichandran <ajay.ravichandran@freshworks.com>
2021-04-27 09:00:23 +0530

    Spring DI (#2)
    * Adding dependencies
    
    * Changing parent mods

--- springdemo/src/com/aj/springdemo/TrackCoach.java
+++ springdemo/src/com/aj/springdemo/TrackCoach.java
@@ -2,8 +2,19 @@ package com.aj.springdemo;
 
 public class TrackCoach implements Coach {
 
+    private FortuneService fortuneService;
+
+    public TrackCoach(FortuneService fortuneService) {
+        this.fortuneService = fortuneService;
+    }
+
     @Override
     public String getDailyWorkout() {
         return "Run for 5km.";
+    }
+
+    @Override
+    public String getDailyFortune() {
+        return "Just do it: " + fortuneService.getFortune();
     }
 }
