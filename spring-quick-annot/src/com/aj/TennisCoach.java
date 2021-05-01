593c4477711a101477644b6c49fe7b43d9c46076
Ajay <ajay.ravichandran@freshworks.com>
2021-05-01 11:58:09 +0530

    changing ioc

--- spring-quick-annot/src/com/aj/TennisCoach.java
+++ spring-quick-annot/src/com/aj/TennisCoach.java
@@ -1,11 +1,38 @@
 package com.aj;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-@Component("sampleCoach") //spring will automatically register this bean with the container
+@Component //spring will automatically register this bean with the container
 public class TennisCoach implements Coach{
+
+    private FortuneService fortuneService;
+
+//    @Autowired //spring will scan for a component that implements the fortune service interface
+//    public TennisCoach(FortuneService fortuneService) {
+//        this.fortuneService = fortuneService;
+//    }
+
+    //define default constructor
+
+    public TennisCoach(){
+        System.out.println("Inside default constructor");
+    }
+
+    //creating setter method
+    @Autowired
+    public void setFortuneService(FortuneService fortuneService) {
+        System.out.println("Inside Setter");
+        this.fortuneService = fortuneService;
+    }
+
     @Override
     public String getDailyWorkout() {
         return "practice volley";
+    }
+
+    @Override
+    public String getDailyFortune() {
+        return fortuneService.getFortune();
     }
 }
